cf5fde53040af547f907345ab34c8eb0e58c8d64
Julio Daal Sousa <32969470+juliodaal@users.noreply.github.com>
2020-08-31 17:24:09 +0100

    Fixing .env credentials error

---
+++ app/Http/Controllers/ResetPassword.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Http\Controllers\EmailController;
+use App\Http\Controllers\FileAdminDataController;
+use App\User;
+use Exception;
+
+class ResetPassword extends Controller
+{
+    public function forgotPassword(Request $request){
+        try {
+            $name = User::where('email',$request->email)->first();
+            $pass = Str::random(9);
+            $data = new EmailController($name->name,$request->email,$pass);
+            $data->sendEmailPassword();
+        } catch (\Exception $e) {
+            return FileAdminDataController::reportError('/home',$e);
+        }
+        return redirect('/')->with('successfully', 'Senha enviada ao Email ' . $request->email);
+    }
+}
